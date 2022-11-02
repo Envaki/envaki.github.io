8dffff90de48eb40c12854cb6c5efa47fd34535b
Envaki <117283496+Envaki@users.noreply.github.com>
2022-11-02 18:30:40 +0200

    Update js.js

--- js.js
+++ js.js
@@ -5,7 +5,7 @@ let k = document.querySelector('.open')
 
 function red() {
     let r = document.querySelector('.body')
-    r.style.background = 'url(https://envaki.github.io/img/img1.jpg) no-repeat center/100%'
+    r.style.background = 'url(https://envaki.github.io/blob/main/img/img1.jpg) no-repeat center/100%'
     k.style.border = '4px solid red'
     k.style.color = 'red'
     x.style.background = "green"
